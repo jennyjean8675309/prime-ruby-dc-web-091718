4b242ad9b961006886c23c72ca640a1927f2c81d
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-29 21:19:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,9 @@ def prime?(integer)
   test_array = test_factors.to_a
   modulo_remainders = []
   test_array.each {|i| modulo_remainders.push(integer % i)}
-  if integer < 2
+  prime_check = modulo_remainders.size
+  if prime_check == 1 && integer >= 2
+    true
+  else 
     false
-  elsif integer >= 2
-  check_for_float = []
-  divided_integers.each do |n|
-    check_for_float.push(n.is_a?(Integer))
-    if check_for_float.include?(Integer)
-      false
-    else true
-    end
-  end
-end end
+end
