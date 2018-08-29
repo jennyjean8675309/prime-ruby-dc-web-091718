0c5e394345fbc768748e3920ff1660f9c253e22c
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-29 21:28:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 def prime?(integer)
   test_factors = (2..integer)
   test_array = test_factors.to_a
-  modulo_remainders = []
-  test_array.each {|i| modulo_remainders.push(integer % i)}
-  prime_check = modulo_remainders.size
-  if prime_check == 1 && integer >= 2
+  
     true
   else 
     false
