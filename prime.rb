3d2492e9fba1dfa258ee7c3c7b4023a68f17a443
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-29 20:29:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,7 @@ def prime?(integer)
   test_array = test_factors.to_a
   divided_integers = []
   test_array.each {|i| divided_integers.push((integer.to_f)/i)}
+  divided_integers
   n = 0
   if (divided_integers[n].is_a?(Integer) == true && divided_integers[n] != integer || integer < 2)
     n += 1
