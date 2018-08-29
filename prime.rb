34362741b313a25fd556d1569797d7a014ddd801
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-29 21:36:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 def prime?(integer)
   test_factors = (2..integer)
   test_array = test_factors.to_a
-  
+  modulo_operators = []
+  test_array.each do |i|
+    modulo_operators.push(integer % i)
+  end
+  if modulo_operators[0] == 0
     true
   else 
     false
