b73086c0f9ba478779228fad39542b00b5dd7388
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-29 20:32:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,9 @@ def prime?(integer)
   test_array.each {|i| divided_integers.push((integer.to_f)/i)}
   divided_integers
   n = 0
-  if (divided_integers[n].is_a?(Integer) == true && divided_integers[n] != integer || integer < 2)
+  if integer < 2
+    false
+  elsif (divided_integers[n].is_a?(Integer) == true && divided_integers[n] != integer)
     false
     n += 1
   else
